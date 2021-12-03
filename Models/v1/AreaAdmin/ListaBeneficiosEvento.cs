c9af3046f97507ff5a300fd2090d38c7d341d262
Diego Portela <didiego1903@gmail.com>
2021-12-02 23:16:05 -0300

    Fazendo correção do nome da classe ListaBeneficiosEvento para ListarBeneficiosEvento

--- Models/v1/AreaAdmin/ListaBeneficiosEvento.cs
+++ Models/v1/AreaAdmin/ListaBeneficiosEvento.cs
@@ -3,7 +3,7 @@ using BoschCartaoDigitalBackEnd.Models.v1.ProjetoBoschContext;
 
 namespace BoschCartaoDigitalBackEnd.Models.v1.AreaAdmin
 {
-    public class ListaBeneficiosEvento
+    public class ListarBeneficiosEvento
     {
         public List<Beneficio> Beneficios { get; set; }
     }
