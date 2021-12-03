725b9ef8ce941112499b4d4db0692e0bc504e0e9
Diego Portela <didiego1903@gmail.com>
2021-12-03 16:41:43 -0300

    salvando para mudar

--- Models/v1/Responses/AreaAdmin/ListarBeneficiosEventoResponse.cs
+++ Models/v1/Responses/AreaAdmin/ListarBeneficiosEventoResponse.cs
@@ -1,6 +1,7 @@
 using System;
 using System.Collections.Generic;
 using BoschCartaoDigitalBackEnd.Models.v1.AreaPublica.Response;
+using BoschCartaoDigitalBackEnd.Models.v1.ProjetoBoschContext.Response;
 
 namespace BoschCartaoDigitalBackEnd.Models.v1.Responses.AreaAdmin
 {
