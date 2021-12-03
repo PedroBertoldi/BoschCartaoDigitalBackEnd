b339a63f11a35e7b035535d6469b16c491a10042
Diego Portela <didiego1903@gmail.com>
2021-12-02 22:06:42 -0300

    Salvando o novo Models

---
+++ Models/v1/AreaAdmin/ListaBeneficiosEvento.cs
@@ -0,0 +1,10 @@
+using System.Collections.Generic;
+using BoschCartaoDigitalBackEnd.Models.v1.ProjetoBoschContext;
+
+namespace BoschCartaoDigitalBackEnd.Models.v1.AreaAdmin
+{
+    public class ListaBeneficiosEvento
+    {
+        public List<Beneficio> Beneficios { get; set; }
+    }
+}
